4cb6050cbf259b0819dde920f73ae93e3102669b
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-11-21 15:22:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EvenOrOdd;
+
+public class Program
+{
+    private static void PrintEvenNumbersInRange(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+            if (i % 2 == 0)
+                Console.WriteLine($"{i} es par.");
+    }
+
+    public static void Main(string[] args)
+    {
+        PrintEvenNumbersInRange(1, 100);
+    }
+}
